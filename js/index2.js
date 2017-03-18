7174de2eb6b18cbd993fdc114fa76a05c0d17fd0
gongjianpeng <gongjianpeng@jinjixiadan.com>
2017-03-18 16:10:40 +0800

    瀑布流

---
+++ js/index2.js
@@ -0,0 +1,6 @@
+$(function(){
+		
+		for (var i=0;i<10;i++){
+			$("<div class='box'><div class='info'><div class='pic'><img src='images/"+(i+1)+".jpg' /></div></div></div>").appendTo($("#wrap"));
+	}
+})
